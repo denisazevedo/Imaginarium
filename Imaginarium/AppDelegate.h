df22f0e9d108d1f42dce1388af814796637caa03
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-02-24 19:12:00 -0300

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Denis C de Azevedo on 24/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Imaginarium/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by Denis C de Azevedo on 24/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
